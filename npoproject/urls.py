e4b174a2235ce8354c469660a36a3c9558570d81
Andrew <goodman.drew1@gmail.com>
2024-09-06 10:45:05 -0500

    added logout

--- npoproject/urls.py
+++ npoproject/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from npoapi.views.user_view import UserViewSet  # Import from views package
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r"users", UserViewSet, basename="user")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
-
